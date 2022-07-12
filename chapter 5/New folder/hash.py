19dbff83290f0c2a06a4a6f063e914d1bc65d51c
amr12601 <62963089+amr12601@users.noreply.github.com>
2022-07-12 08:13:06 +0200

    hash tables

--- chapter 5/New folder/hash.py
+++ chapter 5/New folder/hash.py
@@ -24,5 +24,41 @@ def check_voter(name):
         voted[name] = True
         print("let " + name + " vote")
 
-check_voter("amr")
-check_voter("Gharam")
+check_voter("amr") # kick amr off
+check_voter("Gharam") # let gharam vote
+
+
+# hash tables are used as a cache (remembering the data instead of recalculating it)
+# it has 2 advantages :
+# 1 --> you get the web page a lot faster
+# 2 -> servers have to do less work
+
+server = {"facebook.com":"Data for fb home page", "facebook.com/about":"data for the about page", "youtube.com":"youtube home page",
+           "goodreads.com":"good reads home page"}
+           
+cached = {}
+cached["facebook.com"] = "Data for fb home page" # cached before
+
+def get_data_from_servers(url):
+    return server[url]
+   
+
+def cache(url):
+    if cached.get(url):
+        print(url + " is in cache")
+        return cached[url]
+    else:
+        print("get " + url + " from servers")
+        data = get_data_from_servers(url)
+        cached[url] = data
+        return data
+       
+       
+print(cache("facebook.com") + "\n")
+print(cache("facebook.com/about") + "\n")
+print(cache("goodreads.com") + "\n")
+
+
+
+
+
